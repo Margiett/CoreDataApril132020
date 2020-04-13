8f3de103c880d87f184766784bdc2ee95835430b
Margiett Gil <mxg627@icloud.com>
2020-04-13 17:18:49 -0400

    CoreData

---
+++ CoreDataApril132020/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataApril132020
+//
+//  Created by Margiett Gil on 4/13/20.
+//  Copyright © 2020 Margiett Gil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
